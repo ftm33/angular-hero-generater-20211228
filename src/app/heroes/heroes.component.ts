5363067610ded875b422e3b9be1f2b13947c3383
ftm33 <814400646jzd@gmail.com>
2021-12-28 19:02:22 +0900

    3 files modified, 2 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
   constructor() {}
-  hero = 'aaa';
+  heroes = HEROES;
+  selectedHero: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
   ngOnInit() {}
 }
